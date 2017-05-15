f7e0e3f09d8119a898bafea9555180020f7821fd
joelless <joellessimonet@gmail.com>
2017-05-15 17:17:59 +0200

    calculation_service updated: predictions possible from console

--- app/services/calculation_service.rb
+++ app/services/calculation_service.rb
@@ -28,31 +28,47 @@ class CalculationService
 		model = TfIdfSimilarity::TfIdfModel.new(documents, :library => :narray)
 		matrix = model.similarity_matrix
 
-		calculatePredictions(user_id, matrix)
+		calculatePredictions(user_id, documents, model, matrix)
 
 		#puts matrix[model.document_index(documents[2]), model.document_index(documents[3])]
 	end
 
-	def calculatePredictions(user_id, matrix)
-		predictions = []
+	def calculatePredictions(user_id, documents, model, matrix)
+		puts user_id
+		predictions = {}
 		user = User.find(user_id)
+		#puts user
 		rated_courses = user.users_courses
+		#puts rated_courses
+		#puts documents.size
 		all_courses = Course.all
-		all_courses.find_each do |course|
-			#if course.grade
-			#num = matrix[,] * rated_courses.find(:)
-			#denum =
-			#puts course.grade
-			#puts course.course_id
-			#end
+		#loop through every course to calculate predictions for the user
+		all_courses.find_each.with_index do |item, i_index|
+			num = 0
+			denum = 0
+			#puts i_index
+			rated_courses.find_each.with_index do |j, j_index|
+				#puts "in rated courses j each"
+				#puts j
+				#puts j_index
+				#puts j.grade.to_s
+				if model.document_index(documents[item.id]) && model.document_index(documents[j.course.id])
+					tmp =  matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])] * j.grade.to_f
+					num = num + tmp
+
+					tmp2 = matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])]
+					denum = denum + tmp2
+				end
+			end
+			#puts num
+			#puts denum
+			if denum != 0
+				predictions[item.id] = num/denum
+			end
+
+			#denum = matrix[model.document_index(documents[2]), model.document_index(documents[3])]
+
 		end
-		#puts all_courses
-		#Course.find_each do |course|
-		#	if course.description
-				#puts course.description.to_s
-				#document = TfIdfSimilarity::Document.new(course.description)
-				#documents.push(document)
-		#	end
-		#end
+		puts predictions
 	end
 end
