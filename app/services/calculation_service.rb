73370072687d941c45d24bb316ab2321a5a42bea
joelless <joellessimonet@gmail.com>
2017-05-15 14:24:53 +0200

    calculationservice update

--- app/services/calculation_service.rb
+++ app/services/calculation_service.rb
@@ -3,9 +3,9 @@ class CalculationService
 	# courses = CalculationService.new(descriptions); nil
 	# courses.calculateTfidfMatrix
 
-	attr_reader :some_stuffs
-	def initialize(some_stuffs)
-		@some_stuffs = some_stuffs
+	attr_reader :user_id
+	def initialize(user_id)
+		@user_id = user_id
 	end
 
 	def calculateTfidfMatrix
@@ -13,18 +13,45 @@ class CalculationService
 		require 'tf-idf-similarity'
 		require 'narray'
 		documents = []
-		some_stuffs.each do |stuff|
-			if stuff
-				document = TfIdfSimilarity::Document.new(stuff)
+
+		#puts us_courses
+		all_courses = Course.all
+		#puts all_courses
+		Course.find_each do |course|
+			if course.description
+				#puts course.description.to_s
+				document = TfIdfSimilarity::Document.new(course.description)
 				documents.push(document)
 			end
 		end
-
+		#puts documents[1]
 		model = TfIdfSimilarity::TfIdfModel.new(documents, :library => :narray)
 		matrix = model.similarity_matrix
-		#example with SPSS atelier in german and french
-		puts some_stuffs[2]
-		puts some_stuffs[3]
-		puts matrix[model.document_index(documents[2]), model.document_index(documents[3])]
+
+		calculatePredictions(user_id, matrix)
+
+		#puts matrix[model.document_index(documents[2]), model.document_index(documents[3])]
+	end
+
+	def calculatePredictions(user_id, matrix)
+		predictions = []
+		user = User.find(user_id)
+		rated_courses = user.users_courses
+		all_courses = Course.all
+		all_courses.find_each do |course|
+			#if course.grade
+
+			puts course.grade
+			puts course.course_id
+			#end
+		end
+		#puts all_courses
+		#Course.find_each do |course|
+		#	if course.description
+				#puts course.description.to_s
+				#document = TfIdfSimilarity::Document.new(course.description)
+				#documents.push(document)
+		#	end
+		#end
 	end
 end
