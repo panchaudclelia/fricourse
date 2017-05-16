a9f249d44c179dfc6abf16e0f0cf85987f1619af
joelless <joellessimonet@gmail.com>
2017-05-16 20:21:25 +0200

    recommendations only showing courses of users' study path, if no courses chosen, recommendations based on other peoples grades useremail added to page header

--- app/services/calculation_service.rb
+++ app/services/calculation_service.rb
@@ -35,26 +35,33 @@ class CalculationService
 		puts user_id
 		@recommendations = {}
 		user = User.find(user_id)
+		sp = user.study_paths.first
 		rated_courses = user.users_courses
+		if rated_courses.present? == false
+			rated_courses = UsersCourse.all
+		end
 		#all_courses = Course.all
 		all_courses = Course.where(semester: ['SP 16 (cours semestriel)','FS 16 (Semesterkurs)','SS 16 (Semester Course)'])
 		#loop through current semesters courses to calculate recommendations for the user
 		all_courses.find_each.with_index do |item, i_index|
-			num = 0
-			denum = 0
-			
-			rated_courses.find_each.with_index do |j, j_index|
-				if model.document_index(documents[item.id]) && model.document_index(documents[j.course.id])
-					tmp =  matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])] * j.grade.to_f
-					num = num + tmp
+			#if sp.course_modules.ids.include?(item.belongings.pluck(:course_module_id).first)
+			if sp.course_modules.ids.any? {|i| item.belongings.pluck(:course_module_id).include?(i) }
+				num = 0
+				denum = 0
 
-					tmp2 = matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])]
-					denum = denum + tmp2
+				rated_courses.find_each.with_index do |j, j_index|
+					if model.document_index(documents[item.id]) && model.document_index(documents[j.course.id])
+						tmp =  matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])] * j.grade.to_f
+						num = num + tmp
+
+						tmp2 = matrix[model.document_index(documents[item.id]), model.document_index(documents[j.course.id])]
+						denum = denum + tmp2
+					end
 				end
-			end
 
-			if denum != 0
-				@recommendations[item.id] = num/denum
+				if denum != 0
+					@recommendations[item.id] = num/denum
+				end
 			end
 		end
 		@recommendations
