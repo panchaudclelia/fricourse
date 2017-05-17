1f095f3b20c56871e2c3b0df931bfd3c918673d1
Clélia Panchaud <clelia.panchaud@gmail.com>
2017-05-17 10:47:49 +0200

    grf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username,:first_name, :last_name, :birthdate, :study_path_id])
   end
 
-  def destroy
-  signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-  set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-  yield if block_given?
-  respond_to_on_destroy
-end
 
 def layout_by_resource
     if controller_name == 'sessions' || devise_controller? && !['edit', 'update', 'delete'].include?(action_name)
