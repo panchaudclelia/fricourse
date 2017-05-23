ebfd9c3482560089847aa9fa03352a7d7401f2f0
Clélia Panchaud <clelia.panchaud@gmail.com>
2017-05-23 16:51:06 +0200

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  root 'courses#index'
+  root 'static#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
