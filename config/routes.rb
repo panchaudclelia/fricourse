a2eafcfbb9c4211794374cb2ffa0144cb96a114b
joelless <joellessimonet@gmail.com>
2017-05-23 14:05:33 +0200

    bööö

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :recommendations
   resources :study_paths
-  resources :courses 
-  resources :course_modules 
-  devise_for :users
+  resources :courses
+  resources :course_modules
+  #devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'courses#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
